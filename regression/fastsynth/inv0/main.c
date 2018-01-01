3f466841e0042504bc6da0777cb9906b3099af1d
Daniel Kroening <kroening@cs.ox.ac.uk>
2018-01-01 16:28:52 +0000

    make property easier

--- regression/fastsynth/inv0/main.c
+++ regression/fastsynth/inv0/main.c
@@ -8,6 +8,9 @@ int main()
   x=0;
   __CPROVER_assert(x<EXPRESSION_inv(), "base case");
 
+  // this is a hint
+  __CPROVER_assert(EXPRESSION_inv()<20, "hint");
+
   // step case
   __CPROVER_havoc_object(&x);
 
@@ -17,10 +20,10 @@ int main()
     // do the transition
     x++;
 
-    if(x!=1000)
+    if(x!=10)
     {
       // check the property
-      __CPROVER_assert(x!=2000, "property");
+      __CPROVER_assert(x!=200, "property");
 
       // re-establish invariant
       __CPROVER_assert(x<EXPRESSION_inv(), "step case");    
