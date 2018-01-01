0bb75f69f2f56dc6c9e6b23651273ec13c7ad820
Daniel Kroening <kroening@cs.ox.ac.uk>
2018-01-01 15:40:43 +0000

    explicit nondet

--- regression/fastsynth/constant5/main.c
+++ regression/fastsynth/constant5/main.c
@@ -1,11 +1,14 @@
 int EXPRESSION(void);
+int nondet_int();
 
 int main()
 {
-  int x;
+  int x=nondet_int();
 
   if(x>=901)
     __CPROVER_assert(EXPRESSION()<x, "<901");
   else if(x<=900)
     __CPROVER_assert(EXPRESSION()>=x, ">=900");
+
+  return 0;
 }
