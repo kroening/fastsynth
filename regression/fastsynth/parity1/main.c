3170eaf8d30103f760d5100e1ad7996848ef027d
Daniel Kroening <kroening@cs.ox.ac.uk>
2018-01-03 17:28:13 +0000

    parity test

---
+++ regression/fastsynth/parity1/main.c
@@ -0,0 +1,16 @@
+__CPROVER_bool EXPRESSION_parity(
+  __CPROVER_bool, __CPROVER_bool, __CPROVER_bool, __CPROVER_bool);
+  
+__CPROVER_bool nondet_bool();
+
+int main()
+{
+  __CPROVER_bool a=nondet_bool(),
+                 b=nondet_bool(),
+                 c=nondet_bool(),
+                 d=nondet_bool(), result;
+
+  result = EXPRESSION_parity(a, b, c, d);
+
+  __CPROVER_assert(result == (a^b^c^d), "parity");
+}
