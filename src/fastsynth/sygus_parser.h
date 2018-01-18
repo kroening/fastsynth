2235287f159c94fac01bf7c39f25bd3ef41cf0df
Daniel Kroening <kroening@cs.ox.ac.uk>
2018-01-18 17:34:19 +0000

    record which functions are synth-fun

--- src/fastsynth/sygus_parser.h
+++ src/fastsynth/sygus_parser.h
@@ -1,3 +1,5 @@
+#include <set>
+
 #include "smt2_parser.h"
 
 #include <util/expr.h>
@@ -14,6 +16,8 @@ public:
 
   exprt::operandst constraints;
   std::string logic, action;
+
+  std::set<irep_idt> synth_fun_set;
 
   virtual void error(const std::string &s) override;
 
