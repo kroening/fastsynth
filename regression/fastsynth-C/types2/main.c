b33f85c350f669acb7af6428604608e7b0a22431
Daniel Kroening <kroening@cs.ox.ac.uk>
2018-01-28 21:21:06 +0000

    fix types2 test

--- regression/fastsynth-C/types2/main.c
+++ regression/fastsynth-C/types2/main.c
@@ -1,8 +1,9 @@
 int EXPRESSION(unsigned char);
+unsigned char nondet_unsigned_char();
 
 int main()
 {
-  unsigned char in;
+  unsigned char in=nondet_unsigned_char();
   int out;
   out=EXPRESSION(in);
   // the below needs promotion, or overflows
