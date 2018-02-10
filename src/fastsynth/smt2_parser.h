7da06941a177128ea61ca9c70ca72238d9e29f23
Daniel Kroening <kroening@cs.ox.ac.uk>
2018-02-10 18:48:07 +0000

    move SMT2 tokenizer into separate file

--- src/fastsynth/smt2_parser.h
+++ src/fastsynth/smt2_parser.h
@@ -9,59 +9,13 @@ Author: Daniel Kroening, kroening@kroening.com
 #ifndef CPROVER_SOLVERS_SMT2_SMT2_PARSER_H
 #define CPROVER_SOLVERS_SMT2_SMT2_PARSER_H
 
-#include <util/parser.h>
-
-#include <string>
 #include <util/std_expr.h>
+
+#include "smt2_tokenizer.h"
 
 #include "function.h"
 
 class function_typet;
-
-class smt2_tokenizert:public parsert
-{
-public:
-  explicit smt2_tokenizert(std::istream &_in):
-    ok(true), peeked(false), token(NONE)
-  {
-    in=&_in;
-  }
-
-  operator bool()
-  {
-    return ok;
-  }
-
-protected:
-  std::string buffer;
-  bool ok, peeked;
-  using tokent=enum { NONE, END_OF_FILE, ERROR, STRING_LITERAL,
-                      NUMERAL, SYMBOL, OPEN, CLOSE };
-  tokent token;
-
-  tokent next_token();
-
-  tokent peek()
-  {
-    if(peeked)
-      return token;
-    else
-    {
-      next_token();
-      peeked=true;
-      return token;
-    }
-  }
-
-private:
-  tokent get_decimal_numeral();
-  tokent get_hex_numeral();
-  tokent get_bin_numeral();
-  tokent get_simple_symbol();
-  tokent get_quoted_symbol();
-  tokent get_string_literal();
-  static bool is_simple_symbol_character(char);
-};
 
 class new_smt2_parsert:public smt2_tokenizert
 {
