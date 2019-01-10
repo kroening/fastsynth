69b6fc86d5c73c3499859ce0d7cd0a71240312bf
Daniel Kroening <kroening@cs.ox.ac.uk>
2019-01-10 20:20:22 +0000

    added produce_training_data()

--- src/dnnsynth-train/dnnsynth_train_main.cpp
+++ src/dnnsynth-train/dnnsynth_train_main.cpp
@@ -23,29 +23,39 @@ public:
   }
 };
 
+struct datat
+{
+  torch::Tensor data, labal;
+};
+
+std::vector<datat> produce_training_data()
+{
+  std::vector<datat> result;
+
+  return result;
+}
+
 void train(
   const std::size_t epoch,
   synth_net &net,
+  const std::vector<datat> &training_data,
   torch::optim::SGD &optimizer)
 {
   size_t batch_index = 0;
 
-  for(unsigned i=0; i<=20; i++, batch_index++)
+  for(unsigned i=0; i<training_data.size(); i++, batch_index++)
   {
     // reset gradients
     optimizer.zero_grad();
 
-    // produce data
-    torch::Tensor data = torch::zeros(8);
-    torch::Tensor label = torch::zeros(1);
-
-    data[0] = (int64_t)batch_index;
+    // get the data
+    const auto &data = training_data[batch_index];
 
     // Execute the model on the input data
-    auto prediction = net.forward(data);
+    auto prediction = net.forward(data.input);
 
     // Compute loss value
-    auto loss = torch::binary_cross_entropy(prediction, label);
+    auto loss = torch::binary_cross_entropy(prediction, data.label);
 
     // Compute gradients of the loss
     loss.backward();
@@ -70,9 +80,12 @@ int main()
 
   torch::optim::SGD optimizer(net.parameters(), /*lr=*/0.1);
 
+  std::vector<datat> training_data =
+    produce_training_data();
+
   for(size_t epoch = 1; epoch <= 3; ++epoch)
   {
-    train(epoch, net, optimizer);
+    train(epoch, net, training_data optimizer);
   }
 
   // Serialize the net
